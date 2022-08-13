c67c14c421f74652f3e1c00e902dc1a665bcbb6c
Ayushi Agarwal <95530356+agarwalayushi2102@users.noreply.github.com>
2022-08-13 13:01:46 +0530

    Debugged routes error

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,22 +4,30 @@ const services = require('../services/render');
 const Patient = require('../model/patient_model');
 const Appointment = require("../model/appointment_model");
 const Doctor = require("../model/doctor_model");
+const Login = require("../../views/login/homeSchema");
 
 //adding the html files
 router.get('/', (req, res) => {
     res.render('homepage')
 });
-router.get('/login', (req, res) => {
+router.get('/blogs', (req, res) => {
+    res.render('blogs')
+});
+router.get('/contact', (req, res) => {
+    res.render('Contact')
+});
+router.get('/login', async (req, res) => {
+
     res.render('login')
 });
-router.get('/appointments', async(req, res) => {
+router.get('/appointments', async (req, res) => {
     const doctors = await Doctor.find();
-    res.render('booking', {doctors:doctors})
+    res.render('booking', {doctors: doctors})
 });
 
-router.get('/appointments/:doctor_id', async(req, res) => {
+router.get('/appointments/:doctor_id', async (req, res) => {
     const doctor = await Doctor.findById(req.params.doctor_id);
-    res.render('bookingForm', {doctor:doctor})
+    res.render('bookingForm', {doctor: doctor})
 });
 
 module.exports = router;
